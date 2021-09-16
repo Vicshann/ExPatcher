e16103284b9772cfe6979bf92637e26917108a1e
Victor Sheinmann <Vicshann@gmail.com>
2021-09-16 14:09:55 +0500

    Memory allocation changed

--- ExPatcher.cpp
+++ ExPatcher.cpp
@@ -18,17 +18,21 @@ void _stdcall SysMain(DWORD UnkArg)
  lstrcpyW(StartUpDir, ExePath);  
  TrimFilePath(StartUpDir);     // This causes x64dbg to crash in 'dbghelp.dll!AddressMap::getSectionLength(unsigned long)' while loading pdb of this EXE
  LogMode = lmCons;
+#ifdef _DEBUG
+ LogMode |= lmFile;
+ wsprintfW(LogFilePath, L"%s.%08X.log",&ExePath,GetTickCount());
+#endif
 
  int ForcePatch = false;
  PWSTR CmdLine = GetCommandLineW();
- DBGMSG("CmdLine: %ls", &CmdLine);
+ DBGMSG("CmdLine: %ls", CmdLine);
  CmdLine = GetCmdLineParam(CmdLine, PWSTR(0));   // Skip EXE file name and path
  int ParCnt = -1;
  wchar_t Cmd[64];
  wchar_t Arg[MAX_PATH];
  for(;*CmdLine;ParCnt++)
   {
-   DBGMSG("Parsing Arg: %ls", &CmdLine);
+   DBGMSG("Parsing Arg: %ls", CmdLine);
    CmdLine = GetCmdLineParam(CmdLine, Cmd);
    if(NSTR::IsStrEqualIC("-F", Cmd))
     {
@@ -76,28 +80,35 @@ void _stdcall SysMain(DWORD UnkArg)
    }
  NSIGP::CSigPatch patch;
 
- HANDLE hFile   = CreateFileX(&ScrtFile,GENERIC_READ,FILE_SHARE_READ|FILE_SHARE_WRITE,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL|FILE_FLAG_SEQUENTIAL_SCAN,NULL);
+ HANDLE hFile   = CreateFileX(&ScrtFile,GENERIC_READ,FILE_SHARE_READ|FILE_SHARE_WRITE,NULL,OPEN_EXISTING,FILE_FLAG_SEQUENTIAL_SCAN,NULL);
  if(hFile == INVALID_HANDLE_VALUE){OUTMSG("Failed to open the script file!"); ExitProcess(-1);}
  DWORD Result   = 0;
  DWORD FileSize = GetFileSize(hFile,NULL);
- PBYTE DataBuf  = (PBYTE)HeapAlloc(GetProcessHeap(),HEAP_ZERO_MEMORY,FileSize+64);   // No need to free it
+ PBYTE DataBuf  = (PBYTE)VirtualAlloc(NULL,FileSize+64,MEM_COMMIT,PAGE_READWRITE);  // HeapAlloc(GetProcessHeap(),HEAP_ZERO_MEMORY,FileSize+64);   // No need to free it
  if(FileSize)ReadFile(hFile,DataBuf,FileSize,&Result,NULL);
  CloseHandle(hFile);
 
  if(patch.LoadPatchScript((LPSTR)DataBuf, FileSize) >= 0)
   {
-   hFile    = CreateFileX(&InptFile,GENERIC_READ,FILE_SHARE_READ|FILE_SHARE_WRITE,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL|FILE_FLAG_SEQUENTIAL_SCAN,NULL);
+   hFile    = CreateFileX(&InptFile,GENERIC_READ|GENERIC_WRITE,FILE_SHARE_READ|FILE_SHARE_WRITE,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL|FILE_FLAG_SEQUENTIAL_SCAN,NULL);  // Cannot work with > 4G files on x32!
    if(hFile == INVALID_HANDLE_VALUE){OUTMSG("Failed to open the input file!"); ExitProcess(-2);}
-   Result   = 0;
-   FileSize = GetFileSize(hFile,NULL);
-   DataBuf  = (PBYTE)HeapAlloc(GetProcessHeap(),HEAP_ZERO_MEMORY,FileSize+64);   // No need to free it
-   if(FileSize)ReadFile(hFile,DataBuf,FileSize,&Result,NULL);
+   LARGE_INTEGER FileSize = {0};
+   GetFileSizeEx(hFile, &FileSize);  //GetFileSize(hFile,NULL);
+   HANDLE hMapped = CreateFileMapping(hFile,NULL,PAGE_READWRITE,0,0,NULL);
+   if(!hMapped){OUTMSG("Failed to create the file mapping!"); ExitProcess(0);}
+   PVOID MappedSection = MapViewOfFile(hMapped,FILE_MAP_READ|FILE_MAP_WRITE,0,0,FileSize.QuadPart);
+   if(!MappedSection){OUTMSG("Failed to map the file!"); ExitProcess(0);}
+   OUTMSG("");
+   OUTMSG("Patching...");
+   if(patch.ApplyPatches(MappedSection,FileSize.QuadPart,1,NSIGP::pfForce * ForcePatch) < 0){OUTMSG("Failed to patch the file!"); ExitProcess(0);}
+   FlushViewOfFile(MappedSection, 0);
+   UnmapViewOfFile(MappedSection);
+   CloseHandle(hMapped);
    CloseHandle(hFile);
-   if(patch.ApplyPatches(DataBuf,FileSize,1,NSIGP::pfForce * ForcePatch) < 0){OUTMSG("Failed to patch the file!"); ExitProcess(0);}
-   hFile = CreateFileX(&OutpFile,GENERIC_WRITE,FILE_SHARE_READ|FILE_SHARE_WRITE,NULL,CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL|FILE_FLAG_SEQUENTIAL_SCAN,NULL);
-   if(hFile == INVALID_HANDLE_VALUE){OUTMSG("Failed to create the output file!"); ExitProcess(-3);}
-   WriteFile(hFile,DataBuf,FileSize,&Result,NULL);
-   CloseHandle(hFile);  
+  // hFile = CreateFileX(&OutpFile,GENERIC_WRITE,FILE_SHARE_READ|FILE_SHARE_WRITE,NULL,CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL|FILE_FLAG_SEQUENTIAL_SCAN,NULL);
+  // if(hFile == INVALID_HANDLE_VALUE){OUTMSG("Failed to create the output file!"); ExitProcess(-3);}
+  // WriteFile(hFile,DataBuf,FileSize.QuadPart,&Result,NULL);
+  // CloseHandle(hFile);  
    OUTMSG("Saved: %ls", &OutpFile);
   }
    else {OUTMSG("Failed to parse the Patch Script!");}
